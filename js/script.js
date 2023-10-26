7c5fab3f300dc514a6c9515066664d20db139ebf
pmashi <nocsillty@gmail.com>
2023-10-26 11:05:51 -0400

    draft #2

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ function createGrid() {
 function initialize() { 
     let board = document.getElementById('board');
 
-    for(i=0; i<grid.length; i++) { 
+    for(i=0; i<4; i++) { 
         let row = document.createElement('div');
         row.className = 'row';
         for(k=0; k<4; k++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
             cell.id = i + ", " + k;
-            cell.setAttribute('src', "img/sandbucket.png");
+            // cell.setAttribute('src', "img/sandbucket.png");
+            cell.innerHTML = i + ", " + k;
             cell.addEventListener('click', () => {handleClick(cell)});
             row.appendChild(cell);
         }
@@ -24,7 +25,7 @@ function initialize() {
 
 function render() { 
     for(let cell of document.getElementsByClassName('cell')) { 
-        cell.innerHTML = i + ", " + k; 
+         
     }
 }
 
