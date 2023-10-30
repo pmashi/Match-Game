6e8b9a31a64290d55d6880b4ec37fce442ea6a57
pmashi <nocsillty@gmail.com>
2023-10-30 11:00:20 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 function createGrid() { 
-    
+    let grid = new Array(16);
+    for(let i = 1; i < grid.length; i+=2) { 
+        grid[i-1] = i
+        grid[i] = i; 
+    }
 }
 
 function initialize() { 
     let board = document.getElementById('board');
-
     for(i=0; i<4; i++) { 
         let row = document.createElement('div');
         row.className = 'row';
         for(k=0; k<4; k++) {
-            let cell = document.createElement('div');
+            let cell = document.createElement('img');
             cell.className = 'cell';
-            cell.id = i + ", " + k;
-            // cell.setAttribute('src', "img/sandbucket.png");
-            cell.innerHTML = i + ", " + k;
+            cell.id = i + "," + k;
+            cell.innerHTML = i + "," + k;
+            cell.setAttribute("src", "../img/sandbucket.png");
             cell.addEventListener('click', () => {handleClick(cell)});
             row.appendChild(cell);
         }
         board.appendChild(row);
     }
-
     render(); 
 }
 
